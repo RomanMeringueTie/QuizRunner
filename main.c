764554a3403b79001adb4e4207022fa1936653af
Roman Kochetkov <romankochetkoveducation@gmail.com>
2023-03-21 20:49:49 +0700

    Write a temporary version of the main function in main.c

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 
 int testRun()
 {
+    int testNumber;
+    scanf("%d", &testNumber);
     return 0;
 }
 
@@ -13,5 +15,18 @@ int createTest()
 
 int main()
 {
+    printf("Доступные команды:\n");
+    printf("Создание теста: 1\n");
+    printf("Прохождение теста: 2\n");
+    int command;
+    scanf("%d", &command);
+    if (command == 1)
+    {
+        createTest();
+    }
+    if (command == 2)
+    {
+        testRun();
+    }
     return 0;
 }
